ab374987cd3fc295686bb42e50c914fc6fda39b2
SeonHyungJo <seonhyung.jo@gmail.com>
2020-01-25 00:34:49 +0900

    🖼️ snyung / update / 예제사진 변경

--- example/src/App.js
+++ example/src/App.js
@@ -7,27 +7,27 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <img src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <img src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <img src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
-        <LazyImg src='https://source.unsplash.com/random/400x300' className={"img"}/>
+        <img src='https://images.unsplash.com/photo-1578763713067-ab058f88be3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <img src='https://images.unsplash.com/photo-1578483001364-b508612cde4a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <img src='https://images.unsplash.com/photo-1579783135819-bbc4905aa3a5?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579424393047-42e7bea999a7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1577715148316-b2f37ad59add?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578715106175-cbd8e19101bc?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578773729181-e2ac1c46d990?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578244862681-589872caef6a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579700511188-d3aa73a5a810?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579278229305-260d0f3aa831?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578696871607-c970865615e8?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579439965079-d6571517ba65?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578922582878-ee56ea213f53?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579718417789-3c1826e6d6c2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578441895065-74276720f12d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578126074645-2b0affa37c0c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1577783160334-009acf779c34?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1577486092185-fed43d2e4bb9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579032738751-537bba768b33?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1578374173694-78603911a0c1?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
+        <LazyImg src='https://images.unsplash.com/photo-1579706966804-be9f2bf0b2eb?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=400' className={"img"}/>
       </div>
     )
   }
